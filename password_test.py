936cf05f786313bfaa29498447ba29d5a7077d52
Demarillac Izere <izered3@gmail.com>
2020-11-14 12:19:59 +0200

    created a setup test method

--- password_test.py
+++ password_test.py
@@ -10,3 +10,10 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("","")
+
