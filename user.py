e719c4290017d0ef1f50eba91475830b13f200e3
Demarillac Izere <izered3@gmail.com>
2020-11-14 11:51:46 +0200

    created a user class

---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    """ 
+    A class that generates a passsword locker account credentials (username and password)
+    """
+    users_list=[]
+    
+    def __init__(self,username,password):
+        self.username=name
+        self.password=password
