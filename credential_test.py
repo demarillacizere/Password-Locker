7d72df53343f781d0a6c97ac77d5fbcc9b111fee
Demarillac Izere <izered3@gmail.com>
2020-11-14 14:30:30 +0200

    added a credentials test class

---
+++ credential_test.py
@@ -0,0 +1,12 @@
+import unittest
+from credential import Credential
+import pyperclip
+
+class TestCredentials(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
