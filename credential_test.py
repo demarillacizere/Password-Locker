c428c4a1d56308714213ffcdcbb0035c330b7d21
Demarillac Izere <izered3@gmail.com>
2020-11-14 14:39:44 +0200

    added a setup test method

--- credential_test.py
+++ credential_test.py
@@ -10,3 +10,9 @@ class TestCredentials(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credential("","",'') # create contact object
