4c8e54e22448bb5484ca841871d496919158e0af
Demarillac Izere <izered3@gmail.com>
2020-11-14 14:45:44 +0200

    added a save credential test method

--- credential_test.py
+++ credential_test.py
@@ -15,4 +15,26 @@ class TestCredentials(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_credential = Credential("","",'') # create contact object
+        self.new_credential = Credential("","",'')
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credential.account,"")
+        self.assertEqual(self.new_credential.username,"")
+        self.assertEqual(self.new_credential.password,"")
+    def test_save_credential(self):
+        '''
+        test_save_credential test case to test if the credential object is saved into
+         the contact list
+        '''
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),1)
+    # setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credential.credential_list = []
