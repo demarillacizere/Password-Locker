c69cd6c053eaedb60189b921995e3f924167d2ff
Demarillac Izere <izered3@gmail.com>
2020-11-14 11:57:41 +0200

    created a save user method

--- user.py
+++ user.py
@@ -3,7 +3,14 @@ class User:
     A class that generates a passsword locker account credentials (username and password)
     """
     users_list=[]
-    
+
     def __init__(self,username,password):
         self.username=name
         self.password=password
+
+    def save_user(self):
+         '''
+        save_user method saves user account credentials into users_list
+        '''
+
+        User.users_list.append(self)   
