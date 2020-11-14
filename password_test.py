d3cd6b335368685b742586bd78afe5e74e0f57f9
Demarillac Izere <izered3@gmail.com>
2020-11-14 12:07:41 +0200

    created a test user class

---
+++ password_test.py
@@ -0,0 +1,12 @@
+import unittest 
+from user import User
+import pyperclip
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
