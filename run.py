9280bced16a177d0dc9fc8a4725a9298d1b4aa22
Demarillac Izere <izered3@gmail.com>
2020-11-14 13:09:49 +0200

    created create and save accounts method

---
+++ run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.6
+from user import User
+def create_account(fname,lname,phone,email):
+    '''
+    Function to create a new account
+    '''
+    new_user = User(username,password)
+    return new_user
+def save_account(user):
+    '''
+    Function to save new user
+    '''
+    User.save_user()
+
+print("Welcome to Password Locker")
+print('What is your name')
+new_user.username=input()
+print ("")
